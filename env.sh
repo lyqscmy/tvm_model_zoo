6c3c20294deea1d12d6802c0cf4b306625c9bcc5
Li Yongqiang <lyqscmy@163.com>
2018-05-04 08:31:44 +0000

    add LD_LIBRARY_PATH

--- env.sh
+++ env.sh
@@ -1,2 +1,6 @@
 source ~/venv/nnvm/bin/activate
-export PYTHONPATH=$HOME/git/nnvm/tvm/python:$HOME/git/nnvm/tvm/topi/python:$HOME/git/nnvm/python
+NNVMROOT=$HOME/git/nnvm
+TVMROOT=$HOME/git/nnvm/tvm
+export LD_LIBRARY_PATH=$TVMROOT/lib:${LD_LIBRARY_PATH}
+export DYLD_LIBRARY_PATH=$TVMROOT/lib:${DYLD_LIBRARY_PATH}
+export PYTHONPATH=$TVMROOT/python:$TVMROOT/topi/python:$NNVMROOT/python
